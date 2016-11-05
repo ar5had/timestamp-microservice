657f674690a50657549f5b6d8e1afe08816aa076
Arshad Khan <arshdkhn1@gmail.com>
2016-11-05 00:25:11 +0000

    adding some xtra stuff

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/:date', function(req, res){
   var options = { year: 'numeric', month: 'long', day: 'numeric' };
   var date = isNaN(req.params.date) ? new Date((req.params.date)): new Date(Number(req.params.date));
   var json = date.getTime() ? { 'unix': date.getTime(), 'natural': date.toLocaleDateString("en-US", options) } : { 'unix': null, 'natural': null } ;
-  res.send('<style>body{background: #fefefe;} p{font-weight: 700; text-align: center; font-family: monospace; word-wrap: break-word; color: #625e98; font-size: 25px; margin-top: 40vh;}</style><p>' + JSON.stringify(json) + '</p>');
+  res.send('<style>body{background: #fefefe;} p{font-weight: 500; text-align: center; font-family: monospace; word-wrap: break-word; color: #625e98; font-size: 25px; margin-top: 40vh;}</style><p>' + JSON.stringify(json) + '</p>');
 });
 
 app.listen(app.get('port'), function() {
